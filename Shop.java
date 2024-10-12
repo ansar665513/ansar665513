37c23b77823c49c3366b8b77d89825063cbbbd37
ansar6655 <ansar.ilkin@gmail.com>
2024-10-12 15:58:29 +0300

    first commit

---
+++ Shop.java
@@ -0,0 +1,88 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+/*
+Это программа создает список продуктов перед походом в магазин
+*/
+class Shop {
+   
+
+    // Массив типа строка, куда будет добавлен список с продуктами
+    private List<String> shopList = new ArrayList<>();
+
+    //Это переменная типа строка, в которую будет помещен название продуктами
+    //прочитанная с консоли от пользователя
+    private  String line;
+
+    // Это метод, который запрашивает у пользователя название продуктами
+    //и добавляет в наш список.Метод завершит свою работы только в том случае
+    //когда пользователь введет слово "exit"
+    public  void getShowList() {
+        Scanner scanner = new Scanner(System.in);
+
+
+        do {
+
+            System.out.print("Введите название продуктов: ");
+            line = scanner.nextLine();
+            if(!line.equals("exit")){
+                shopList.add(line);
+            };
+        } while (!line.equals("exit"));
+
+
+
+
+
+
+
+
+    }
+
+    // Этот метод показывет наш список на консоли
+    //В дальнейшем улучшу программу, чтобы список можно было отправить на почту
+    public  void showList(){
+
+        System.out.println(shopList);
+    }
+
+    public  void delList(){
+        Scanner scanner = new Scanner(System.in);
+        String lineString;
+        System.out.println("Выберите элемент,который хотите удалить: ");
+        System.out.println(shopList);
+        lineString = scanner.nextLine();
+        shopList.remove(lineString);
+
+    }
+    public  void start(){
+        int rezult;
+        Scanner s = new Scanner(System.in);
+        
+        do {
+            System.out.println("1: Создать список продуктов" + 
+            "2: Удалить продукт из спика" + 
+            "3: Показать список" + 
+            "4: Выйти из программы");
+            rezult = s.nextInt();
+            switch(rezult) {
+                case (1): getShowList();
+                break;
+                case(2): delList();
+                break;
+                case(3): showList();
+                break;
+
+
+
+
+
+            }
+        
+            
+            
+        } while (rezult != 4);
+    }
+
+
+}
